f7eec639099cebd05c0462c0c8fc95329a34e5aa
SemasselImene <imene.semassel@gmail.com>
2026-02-08 09:17:58 +0100

    main

--- src/main/java/com/example/livapp/LivappApplication.java
+++ src/main/java/com/example/livapp/LivappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsApplication {
+public class LivappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JenkinsApplication.class, args);
+        SpringApplication.run(LivappApplication.class, args);
     }
 
 }
